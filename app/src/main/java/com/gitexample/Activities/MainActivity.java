9f93951b19b876a1018447622464b83288d5e2e7
Ram Sakhare <75200776+ramsakhare@users.noreply.github.com>
2021-05-19 14:50:21 +0530

    Update MainActivity.java
    Toast message removed

--- app/src/main/java/com/gitexample/Activities/MainActivity.java
+++ app/src/main/java/com/gitexample/Activities/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Welcome to github", Toast.LENGTH_SHORT).show();
+      
 
     }
 }
